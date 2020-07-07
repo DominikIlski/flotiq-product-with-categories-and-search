605f5460caaf2d71ef19487ff0fbcf4961d5ce0e
Dominik <dominik.franciszek.ilski@gmail.com>
2020-07-07 15:31:16 +0200

    added option property to details

--- src/templates/details.js
+++ src/templates/details.js
@@ -1,55 +1,79 @@
 import React from "react"
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import { graphql} from "gatsby";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const ProductDetails = data => (
-  < Layout >
-
-    <SEO title={data.data.product.name} keywords={[`gatsby`, `application`, `react`]} />
+  <Layout>
+    <SEO
+      title={data.data.product.name}
+      keywords={[`gatsby`, `application`, `react`]}
+    />
     <div className="container details-page">
       <div className="product-details">
         <div className="Product-Screenshot">
-          {data.data.product.productGallery && data.data.product.productGallery.length ?
+          {data.data.product.productGallery &&
+          data.data.product.productGallery.length ? (
             <Tabs>
               {data.data.product.productGallery.map(items => (
                 <TabPanel key={items.id}>
-                  <Tab><img
+                  <Tab>
+                    <img
                       alt={data.data.product.name}
-                      src={`${process.env.GATSBY_FLOTIQ_BASE_URL}/image/1920x0/${items.id}.${items.extension}`} /></Tab>
+                      src={`${process.env.GATSBY_FLOTIQ_BASE_URL}/image/1920x0/${items.id}.${items.extension}`}
+                    />
+                  </Tab>
                 </TabPanel>
               ))}
               <TabList>
                 {data.data.product.productGallery.map(items => (
                   <Tab key={items.id}>
                     <img
-                        alt={data.data.product.name}
-                        src={`${process.env.GATSBY_FLOTIQ_BASE_URL}/image/1920x0/${items.id}.${items.extension}`} />
+                      alt={data.data.product.name}
+                      src={`${process.env.GATSBY_FLOTIQ_BASE_URL}/image/1920x0/${items.id}.${items.extension}`}
+                    />
                   </Tab>
                 ))}
               </TabList>
-            </Tabs> :
+            </Tabs>
+          ) : (
             <div className="no-image">No Image</div>
-          }
-
+          )}
         </div>
         <div>
-          <h2><a href={`/${data.data.product.category[0].slug}`}>{data.data.product.category[0].name}</a> / {data.data.product.name}</h2>
-
+          <h2>
+            <a href={`/${data.data.product.category[0].slug}`}>
+              {data.data.product.category[0].name}
+            </a>{" "}
+            / {data.data.product.name}
+          </h2>
         </div>
+        <div>
+            <span className="price">
+            Weight Option: {data.data.product.option}
+            </span>
+          </div>
         <div className="row buynowinner">
+          
           <div className="col-sm-2">
             <span className="price">Price: ${data.data.product.price}</span>
           </div>
+
           <div className="col-sm-10 text-left">
             <a
               href="/"
               className="Product snipcart-add-item"
               data-item-id={data.data.product.slug}
               data-item-price={data.data.product.price}
-              data-item-image={data.data.product.productImage && data.data.product.productImage[0] ? `${process.env.GATSBY_FLOTIQ_BASE_URL}/image/1920x0/${data.data.product.productImage[0].id}.${data.data.product.productImage[0].extension}` : ""}              data-item-name={data.data.product.name}
+              data-item-image={
+                data.data.product.productImage &&
+                data.data.product.productImage[0]
+                  ? `${process.env.GATSBY_FLOTIQ_BASE_URL}/image/1920x0/${data.data.product.productImage[0].id}.${data.data.product.productImage[0].extension}`
+                  : ""
+              }
+              data-item-name={data.data.product.name}
               data-item-url={`/`}
             >
               <i className="fas fa-tags" />
@@ -57,21 +81,22 @@ const ProductDetails = data => (
             </a>
           </div>
         </div>
+
         <div
           dangerouslySetInnerHTML={{
-            __html: data.data.product.description
+            __html: data.data.product.description,
           }}
         />
       </div>
     </div>
-  </Layout >
+  </Layout>
 )
 
 export default ProductDetails
 
 export const query = graphql`
   query productBySlug($slug: String!) {
-    product(slug: {eq: $slug}) {
+    product(slug: { eq: $slug }) {
       id
       slug
       name
@@ -91,5 +116,5 @@ export const query = graphql`
         slug
       }
     }
-}
+  }
 `
