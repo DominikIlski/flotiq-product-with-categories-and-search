2d5589f0812d77b72a205cc2dadf4e9f48aa7354
Dominik <dominik.franciszek.ilski@gmail.com>
2020-07-13 09:47:25 +0200

    data fetch work correctly now

--- src/pages/search.js
+++ src/pages/search.js
@@ -8,15 +8,15 @@ const CheckBox = ({ item, selectedCat, handleCheck }) => {
     <div>
       <input
         className="details_inner"
-        key={item.Name}
+        key={item.name}
         onChange={() => {
-          handleCheck(item.Name)
+          handleCheck(item.name)
         }}
         type="checkbox"
-        checked={selectedCat.includes(item.Name)}
-        value={item.Name}
+        checked={selectedCat.includes(item.name)}
+        value={item.name}
       />
-      {item.Name}
+      {item.name}
     </div>
   )
 }
@@ -30,7 +30,7 @@ const IndexSearch = ({
   useEffect(() => {
     ;(async () => {
       const allCategories = await fetch(
-        `${process.env.GATSBY_FLOTIQ_BASE_URL}/api/v1/content/coffeecat?order_by=Name`,
+        `${process.env.GATSBY_FLOTIQ_BASE_URL}/api/v1/content/coffeecat?order_by=name`,
         {
           headers: { "x-auth-token": process.env.GATSBY_FLOTIQ_API_KEY },
         }
@@ -68,27 +68,31 @@ const IndexSearch = ({
 const Search = () => {
   const [categories, setCategories] = useState([])
   const [selectedCat, setSelectedCat] = useState([])
-
-  const basicQuerryString = `${process.env.GATSBY_FLOTIQ_BASE_URL}/api/v1/search?q=`
-  let data
+  const [pageData, setPageDate] = useState([])
+  const contentType = 'content_type[]=coffee'
+  const basicQuerryString = `${process.env.GATSBY_FLOTIQ_BASE_URL}/api/v1/search?${contentType}&q=`
   useEffect(() => {
     let selectedIds = []
     for (let i = 0; i < selectedCat.length; i++) {
-      selectedIds.push(categories.find(item => item.Name === selectedCat[i]).id)
+      selectedIds.push(`"${categories.find(item => item.name === selectedCat[i]).id}"`)
     }
+    if(selectedIds.length === 0)
+      selectedIds.push("*")
     ;(async () => {
       {
-        data = await fetch(
+        let data = await fetch(
           `${basicQuerryString}${selectedIds.toString().replace(/,/g, " ")}`,
           {
             headers: { "x-auth-token": process.env.GATSBY_FLOTIQ_API_KEY },
           }
         ).then(res => res.json())
+        console.log(`${basicQuerryString}${selectedIds.toString().replace(/,/g, " ")}`)
         console.log(data)
-
+        setPageDate(data)
       }
     })()
-    console.log(data)
+    console.log(pageData)
+    
   }, [selectedCat])
 
   return (
